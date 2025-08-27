0a8ff198e8e2b48aebfaa2611e2b07ff1ed6b2d7
GertsDev <gertsdev@gmail.com>
2025-08-26 22:30:32 -0400

    Remove .cursor files from tracking

--- src/app/(dashboard)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import UnderDevelopmentPage from './under-development';
-
-export default function DashboardPage() {
-  return (
-    <main className="flex flex-1 flex-col p-4 md:p-6">
-      <UnderDevelopmentPage />
-    </main>
-  );
-}
